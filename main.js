fd7b8c4686ac6d18195a9345d02158765bd2dc61
Diego Salina <diego.salina.dj@gmail.com>
2023-12-05 19:03:02 -0300

    creacion bucle

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+while (true){
 let nombre = prompt('Por favor, ingrese su nombre')
 let cantidad = pedirNum('Ingrese la cantidad de productos que desea comprar')
 let valor = 0
 let total = 0
-    for (let i=0; i < cantidad; i++){
-        let producto = prompt('Ingrese el nombre del producto')
-        valor = pedirNum('Ingrese el valor de ' + '(' + producto + ')')
-        total += valor
-    }
+
+for (let i=0; i < cantidad; i++){
+    let producto = prompt('Ingrese el nombre del producto')
+    valor = pedirNum('Ingrese el valor de ' + '(' + producto + ')')
+    total += valor
+}
     alert('El valor total de su compra es: ' + total)
 
 let monto = prompt('¿Desea abonar en EFECTIVO o TARJETA?').toUpperCase() 
@@ -16,6 +18,9 @@ if (monto == 'EFECTIVO'){
     alert('Muchas gracias ' + nombre + ' por abonar con tarjeta, le enviaremos un link de pago')
 } else {alert('Ingrese una opción correcta')
 }
+
+}
+
 function pedirNum(str){
     let num = Number(prompt(str))
     while (isNaN(num)){
