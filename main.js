32fa7e1c95df413a04b723e507c4c6a80531ff14
Diego Salina <diego.salina.dj@gmail.com>
2023-12-21 16:48:37 -0300

    segunda pre entrega

--- main.js
+++ main.js
@@ -3,19 +3,38 @@ let nombre = prompt('Por favor, ingrese su nombre')
 let cantidad = pedirNum('Ingrese la cantidad de productos que desea comprar')
 let valor = 0
 let total = 0
+let id = 0
+    class Producto {
+        constructor(nombre, precio){
+            this.nombre = nombre
+            this.precio = precio
+            this.id = id++
+        }
+    }
+    let productos = []
 
 for (let i=0; i < cantidad; i++){
     let producto = prompt('Ingrese el nombre del producto')
     valor = pedirNum('Ingrese el valor de ' + '(' + producto + ')')
     total += valor
+    productos.push(new Producto(producto, valor)) 
 }
-    alert('El valor total de su compra es: ' + total)
+let lista_productos = ''
+let cont = 1
+    productos.forEach((elm)=>{
+        lista_productos += cont + ' - ' + elm.nombre + ': $' + elm.precio + '\n'
+        cont++
+    })
+    
+alert(lista_productos + '\n' + 'El valor total de su compra es: $' + total)
 
-let monto = prompt('¿Desea abonar en EFECTIVO o TARJETA?').toUpperCase() 
-if (monto == 'EFECTIVO'){
+let monto = prompt('¿De qué forma va a abonar? (Seleccione la opción utilizando "1, 2 o 3"). \n 1- En EFECTIVO  \n 2- En TARJETA \n 3- No tengo el dinero suficiente').toUpperCase() 
+if (monto == 'EFECTIVO' || monto == '1'){
     alert('Muchas gracias ' + nombre + ' por abonar en efectivo, pase por el local')
-} else if (monto == 'TARJETA'){
+} else if (monto == 'TARJETA' || monto == '2'){
     alert('Muchas gracias ' + nombre + ' por abonar con tarjeta, le enviaremos un link de pago')
+} else if(monto == '3' || monto == 'No tengo el dinero suficiente'){
+    alert('Vuelva a realizar la compra cuando tenga el dinero suficiente')
 } else {alert('Ingrese una opción correcta')
 }
 
